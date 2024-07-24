3b7a6c87af3ac7820936caf4feff30f5328bab87
sebas389 <sebasantoine229@gmail.com>
2024-07-24 01:07:24 -0500

    Cambios

--- api/app.py
+++ api/app.py
@@ -1,31 +1,26 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 from joblib import load
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-# Cargar el modelo y el scaler
-model = load('random_forest_model.joblib')
-scaler = StandardScaler()
+# Cargar el modelo y el escalador
+model = load('api/random_forest_model.joblib')
+scaler = StandardScaler()  # Asumimos que tu escalador está entrenado y guardado en el modelo
 
 def preprocess_input(data):
     df = pd.DataFrame(data, index=[0])
-
-    # Aplica las mismas transformaciones que usaste en tu modelo
+    # Transformaciones necesarias
     df["Bathrooms"] = np.log(df["Bathrooms"] + 1)
     df["Bedrooms"] = np.log(df["Bedrooms"]+ 1)
     df["Area"] = np.log(df["Area"] + 1)
-    df["Floor"] = np.log(df["Floor"] + 1 )
-
-    # Dummy variables para District
+    df["Floor"] = np.log(df["Floor"] + 1)
+    # Variables dummy para District
     districts = ['list_of_districts_used_in_training']
     for district in districts:
         df[district] = 1 if df['District'] == district else 0
-
     df.drop("District", axis=1, inplace=True)
-
     # Escalar los datos
     df_scaled = scaler.transform(df)
     return df_scaled
@@ -39,10 +34,8 @@ def predict():
     data = request.json
     input_data = preprocess_input(data)
     prediction = model.predict(input_data)
-
     predicted_price = prediction[0]
-
     return jsonify({'price': predicted_price})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
